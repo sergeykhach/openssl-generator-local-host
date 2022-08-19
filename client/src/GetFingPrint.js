85fb37ec690eca222ce571577ecc35c701e8fdf7
sergeykhach <sosesallc@gmail.com>
2022-08-19 13:03:30 +0400

    5.patrasta buildi GetFingPrint.js ev SetFingPrint.js web3 problemy lucel em pakage.jsoni mej ira versian jnjelov nor inst anelov

--- client/src/GetFingPrint.js
+++ client/src/GetFingPrint.js
@@ -1,17 +1,10 @@
 
 //import $ from "jquery";
-import Web3 from 'https://cdn.esm.sh/v58/web3@1.6.1/es2021/web3.js';
-//import Web3 from "web3/dist/web3.min.js";
-//const web3 = new Web3('ws://localhost:8546');
-
-//import Web3 from 'web3';
-//const web3 = new Web3('ws://localhost:8546');
-//web3.setProvider(new Web3.providers.WebsocketProvider('ws://localhost:8546'));
+//import Web3 from 'https://cdn.esm.sh/v58/web3@1.6.1/es2021/web3.js';
+import Web3 from 'web3';
 const web3 = new Web3(window.web3.currentProvider);
 console.log(web3);
 
-
-//const { ethereum } = window;
 let contractAddrOwn = "0x2d28Dafd034fAB7eF324Bbb659D669263b326373";
 let abiOwn =  [
 	{
