e4ba74a1d019944e050a243bf2973592c9871544
sergeykhach <sosesallc@gmail.com>
2022-08-26 17:13:38 +0400

    9. qrcodsvgsave.js -i mej encoda anum

--- client/src/qrToSvgSave.js
+++ client/src/qrToSvgSave.js
@@ -1,6 +1,16 @@
 import QRCodeStyling from 'qr-code-styling';
 
 export default function QrCodeToSvgSave(tx){
+  let newpop = {
+    "description": "OnChain NFT SVG", 
+    "external_url": "", 
+    "image": "data:image/svg+xml;base64,<Base64_SVG>", 
+    "name": "OnChainNFT",
+    "attributes": [ {
+      "trait_type": "Certificate",
+      "value": "SelfSignNFTCert"
+    }], 
+  };
   const qrCode = new QRCodeStyling({
     width: `500`,
     height: `500`,
@@ -23,5 +33,14 @@ export default function QrCodeToSvgSave(tx){
 //qrCode.append(document.getElementById("canvassvg"));
 qrCode.download({ name: "qrcode", extension: "svg" });
 
+//taki erku toghy base64encoda anum, aranc dimaci en zrtiki 
+const s = new XMLSerializer().serializeToString(qrCode._svg)
+const encodedData = window.btoa(s);
+console.log(encodedData);
+/*let base= function getImageDataURL(xml) {
+  return "data:image/svg+xml;base64," + btoa(decodeURI(encodeURIComponent(xml)));
+
+}
+console.log(base(qrCode._svg));*/
 };
 
